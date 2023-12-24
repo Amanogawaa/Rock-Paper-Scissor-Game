da70b44859831b69435becaa74d3fd3625f044f8
Estrella <dommolino28@gmail.com>
2023-12-25 02:21:43 +0800

    add game() function (not a loop rn)

--- index.js
+++ index.js
@@ -6,17 +6,46 @@
 
 let computerScore = 0;
 let playerScore = 0;
-let rounds = 1;
+let rounds;
+
+function game() {
+
+    let player = playerChoice("Rock");
+    let computer = getComputerChoice();
+
+    let gameOne = playRound(player, computer);
+    console.log(gameOne);
+    let gameTwo = playRound(player, computer);
+    console.log(gameTwo);
+    let gameThree = playRound(player, computer);
+    console.log(gameThree);
+    let gameFour = playRound(player, computer);
+    console.log(gameFour);
+    let gameFive = playRound(player, computer);
+    console.log(gameFive);
+}
+
+function playerChoice(choice) {
+    switch (choice) {
+        case "Rock":
+            return "Rock";
+        case "Paper":
+            return "Paper";
+        case "Scissor":
+            return "Scissor";
+    }
+
+}
 
 function getComputerChoice() {
-    let random = Math.floor(Math.random() * 3);
+    let random = Math.floor(Math.random() * 3) + 1;
 
     switch (random) {
-        case 0:
-            return "Rock";
         case 1:
-            return "Paper";
+            return "Rock";
         case 2:
+            return "Paper";
+        case 3:
             return "Scissor";
     }
 
@@ -24,26 +53,30 @@ function getComputerChoice() {
 }
 
 function playRound(player, computer) {
-    if (computer != player) {
-        if (
-            (player === "Rock" && computer === "Scissor") ||
-            (player === "Paper" && computer === "Rock") ||
-            (player === "Scissor" && computer === "Paper")) {
-            return "Player Wins";
-        }
-        if (
-            (computer === "Rock" && player === "Scissor") ||
-            (computer === "Paper" && player === "Rock") ||
-            (computer === "Scissor" && player === "Paper")
-        ) {
-            return "Computer Wins";
-        }
-    } else {
+
+    if (player === computer) {
         return "It's a tie!";
     }
+
+    if (
+        (player === "Rock" && computer === "Scissor") ||
+        (player === "Paper" && computer === "Rock") ||
+        (player === "Scissor" && computer === "Paper")) {
+        console.log(`${player} beats ${computer}`);
+        playerScore++;
+        return "Player Wins";
+
+    }
+    if (
+        (computer === "Rock" && player === "Scissor") ||
+        (computer === "Paper" && player === "Rock") ||
+        (computer === "Scissor" && player === "Paper")
+    ) {
+        console.log(`${computer} beats ${player}`);
+        computerScore++;
+        return "Computer Wins";
+    }
+   
 }
 
-let player = "Paper";
-let computer = getComputerChoice();
-let gameOn = playRound(player, computer);
-console.log(gameOn);
+game();
