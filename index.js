e8a270aa0bdb92e7cc910abc6437c14009471788
Estrella <dommolino28@gmail.com>
2023-12-25 06:24:03 +0800

    try different approach

--- index.js
+++ index.js
@@ -3,80 +3,69 @@
     1 = paper
     2 = scissors
 */
+let user = window.prompt("Rock paper scssor? ");
+
+let computerChoice = ["Rock", "Paper", "Scissor"];
 
 let computerScore = 0;
 let playerScore = 0;
-let rounds;
 
-function game() {
+function game(rounds) {
+  let player = playerChoice("Scissor");
+  let computer = getComputerChoice();
 
-    let player = playerChoice("Rock");
-    let computer = getComputerChoice();
+  for (let i = 0; i < rounds; i++) {
+    console.log(playRound(player, computer));
+  }
 
-    let gameOne = playRound(player, computer);
-    console.log(gameOne);
-    let gameTwo = playRound(player, computer);
-    console.log(gameTwo);
-    let gameThree = playRound(player, computer);
-    console.log(gameThree);
-    let gameFour = playRound(player, computer);
-    console.log(gameFour);
-    let gameFive = playRound(player, computer);
-    console.log(gameFive);
+  console.log(`Player score: ${playerScore}`);
+  console.log(`Computer score: ${computerScore}`);
 }
 
 function playerChoice(choice) {
-    switch (choice) {
-        case "Rock":
-            return "Rock";
-        case "Paper":
-            return "Paper";
-        case "Scissor":
-            return "Scissor";
-    }
-
+  switch (choice) {
+    case "Rock":
+      return "Rock";
+    case "Paper":
+      return "Paper";
+    case "Scissor":
+      return "Scissor";
+    default:
+      return "Something went wrong";
+  }
 }
 
 function getComputerChoice() {
-    let random = Math.floor(Math.random() * 3) + 1;
+  let random = getRandom();
+  return computerChoice[random];
+}
 
-    switch (random) {
-        case 1:
-            return "Rock";
-        case 2:
-            return "Paper";
-        case 3:
-            return "Scissor";
-    }
-
-    return random;
+function getRandom() {
+  return Math.floor(Math.random() * computerChoice.length);
 }
 
 function playRound(player, computer) {
+  if (player === computer) {
+    return "It's a tie!";
+  }
 
-    if (player === computer) {
-        return "It's a tie!";
-    }
+  if (
+    (player === "Rock" && computer === "Scissor") ||
+    (player === "Paper" && computer === "Rock") ||
+    (player === "Scissor" && computer === "Paper")
+  ) {
+    playerScore++;
+    return `Player wins! ${player} beats ${computer}`;
+  }
 
-    if (
-        (player === "Rock" && computer === "Scissor") ||
-        (player === "Paper" && computer === "Rock") ||
-        (player === "Scissor" && computer === "Paper")) {
-        console.log(`${player} beats ${computer}`);
-        playerScore++;
-        return "Player Wins";
-
-    }
-    if (
-        (computer === "Rock" && player === "Scissor") ||
-        (computer === "Paper" && player === "Rock") ||
-        (computer === "Scissor" && player === "Paper")
-    ) {
-        console.log(`${computer} beats ${player}`);
-        computerScore++;
-        return "Computer Wins";
-    }
-   
+  if (
+    (computer === "Rock" && player === "Scissor") ||
+    (computer === "Paper" && player === "Rock") ||
+    (computer === "Scissor" && player === "Paper")
+  ) {
+    computerScore++;
+    return `Computer wins! ${computer} beats ${player}`;
+  }
 }
 
-game();
+game(5);
